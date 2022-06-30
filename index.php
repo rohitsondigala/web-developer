84c63702c0289ff0af5464923e80ab94a1455ea0
rohitsondigala <Rohit@sumayinfotech.com>
2022-06-30 11:20:21 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+    <head>
+        <style>
+            .body{
+                background-color: darkblue;
+            }
+            .h1{ 
+                text-align: center;
+                color: aqua;
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+
+
+            }
+
+        </style>
+
+    </head>
+
+    <body class="body">
+
+        <div class="h1"> <?php echo  "<h1> the sondigala </h1>"  ?> </div>
+        
+        <?php echo "<p> hi guys i am the kaal how are you bhosdivalo</p>" ?>
+
+    </body>
+
+
+</html>
+
+<?PHP
+
+    $x = "love";
+    echo ("i " . $x . " you")
+
+?>
+
+
